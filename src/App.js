5d115cd6769630f80d5b1113bb4cff74df1142e6
Andy <andy@Andys-MacBook-Pro.local>
2021-03-05 11:44:03 -0500

    loop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    React.createElement('div', { className: 'App' }, [
+      React.createElement('ul', {}, [
+        React.createElement('li', {}, 'Hello'),
+        React.createElement('li', {}, 'World'),
+      ])
+    ])
+    // <div className="App">
+    //   <ul>
+    //     <li>Hello</li>
+    //     <li>World</li>
+    //   </ul>
+    // </div>
   );
 }
 
