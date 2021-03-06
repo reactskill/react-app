3c92575317b5d7ea92b8d176317f39d0af84e4c3
Andy <andy@Andys-MacBook-Pro.local>
2021-03-06 16:06:30 -0500

    reactive programming

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 class Counter extends React.Component {
+
+  constructor() {
+    super()
+    this.state = { count: 0 }
+  }
+
+  addOne() {
+    const currentCount = this.state.count
+    this.setState({ count: currentCount + 1 })
+  }
+
   render() {
     return (
       <div>
-        <p>Hello</p>
+        <p>current count: <span>{ this.state.count }</span></p>
+        <p>
+          <button onClick={() => this.addOne()}>Add one</button>
+        </p>
       </div>
     )
   }
