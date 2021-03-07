266d76013fa0ed098b79d016fdb2e1906ddd3640
Andy <andy@Andys-MacBook-Pro.local>
2021-03-06 20:45:53 -0500

    to test appearance

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
 
   const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    fetch('/count').then(res => res.json()).then(serverData => {
-      setCount(serverData.count)
-    })
-  }, [])
-
   return (
     <div className="App">
-      <p>current count: <span>{ count }</span></p>
+      <p>current count: <span data-testid="count">{ count }</span></p>
       <button onClick={() => setCount(count + 1)}>add 1</button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('count appears 0', () => {
+  render(<App />)
+  expect(screen.getByText('0')).toBeInTheDocument()
+})
