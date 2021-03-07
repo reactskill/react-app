be6762000f3cbae54f54aad717255d03bc24b257
Andy <andy@Andys-MacBook-Pro.local>
2021-03-06 20:49:23 -0500

    to test click and change

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+
+const click = () => (
+  new MouseEvent('click', {
+    bubbles: true
+  })
+)
 
 test('count appears 0', () => {
   render(<App />)
   expect(screen.getByText('0')).toBeInTheDocument()
+})
+
+test('click button to add count by 1', () => {
+  render(<App />)
+  const beforeClick = screen.getByTestId('count').textContent
+
+  fireEvent(screen.getByText('add 1'),click())
+
+  const afterClick = screen.getByTestId('count').textContent
+  expect(parseInt(afterClick)).toBe(parseInt(beforeClick) + 1)
 })
